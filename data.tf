a6386bcebefa7537998598ba0ef450e89722908a
Oliver Frolovs <oliver@cloud84.dev>
2023-05-17 00:10:50 +0100

    Add data resource to get internal ID of GitHub App

--- data.tf
+++ data.tf
@@ -1,3 +1,7 @@
+data "tfe_github_app_installation" "current" {
+  installation_id = var.github_app_installation_id
+}
+
 data "tfe_workspace" "current" {
   name         = var.TFC_WORKSPACE_NAME
   organization = var.organization
